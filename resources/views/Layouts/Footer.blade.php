302e0cb4a6b0be3015ed23a4be7bacc6fbda8370
LycaMay <infinitech.lycanobleza@gmail.com>
2024-08-13 18:09:45 +0800

    update

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -1,73 +1,69 @@
-<footer>
-    <div class="container" style="margin-top: 6%;">
-    
-
-        <div class="row ">
-            <div class="col-md-4">
-                <div class="footer-logo mb-xl-4">
-                    <img class="mb-xl-4" src="../logo/dental-logo.png" alt="">
-                    <p class="footer-desc">Specialize in delivering comprehensive IT solutions tailored to your business needs. With a strong focus on innovation and excellence, we provide a range of services designed to enhanve your digital presence and streamline your operations.</p>
-                </div>
-                <div class="footer-social-media">
-                    <ul>
-                        <li><a href="https://www.facebook.com/profile.php?id=61562146592005"><i class="fa-brands fa-facebook-f fa-lg"></i></a></li>
-                        <li><a href=""><i class="fa-brands fa-instagram fa-lg"></i></a></li>
-                        <li><a href=""><i class="fa-brands fa-x-twitter fa-lg"></i></a></li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="col-md-4">
-                <div class="footer-links mb-xl-4">
-                    <h2>Quick Links</h2>
-                </div>
-                <div class="footer-quick-links">
-                    <ul>
-                        <li><a href=""><i class="fa-solid fa-play"></i>Home</a></li>
-                        <li><a href=""><i class="fa-solid fa-play"></i>About Us</a></li>
-                        <li><a href=""><i class="fa-solid fa-play"></i>Blogs and News</a></li>
-                        <li><a href=""><i class="fa-solid fa-play"></i>Work With Us</a></li>
-                        <li><a href=""><i class="fa-solid fa-play"></i>Contact Us</a></li>
-                    </ul>
-                </div>
-
-            </div>
-            <div class="col-md-4">
-                <div class="footer-links mb-xl-4">
-                    <h2>Contact Us</h2>
-                </div>
-                <div class="footer-quick-links">
-                    <ul>
-                        <li><a href=""><i class="fa-solid fa-phone"></i>(02) 123 - 456 - 7890</a></li>
-                        <li><a href=""><i class="fa-solid fa-mobile"></i> (63+) 9924401097</a></li>
-                        <li><a href=""><i class="fa-brands fa-viber"></i> (63+) 9924401097</a></li>
-                        <li><a href=""><i class="fa-brands fa-telegram"></i> (63+) 9924401097</a></li>
-                        <li><a href=""><i class="fa-solid fa-envelope"></i>dentalclinic@gmail.com</a></li>
-
-                    </ul>
-                </div>
-
-            </div>
-
+<!-- Site footer -->
+<footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+         <img class="mb-4" src="../logo/dental-logo.png" alt="">
+          <p class="text-justify">Specialize in delivering comprehensive IT solutions tailored to your business needs. With a 
+            strong focus on innovation and excellence, we provide a range of services designed to enhanve your
+             digital presence and streamline your operations.</p>
         </div>
 
+        <div class="col-xs-6 col-md-3">
+          <h6>Quick Links</h6>
+          <ul class="footer-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/dental/about">About Us</a></li>
+            <li><a href="/dental/blog">Blogs and News</a></li>
+            <li><a href="/dental/services">Work with Us</a></li>
+            <li><a href="/dental/contact">Contact Us</a></li>
+          </ul>
+        </div>
 
+        <div class="col-xs-6 col-md-3">
+          <h6>Contact Us</h6>
+          <ul class="footer-links">
+            <li><a href="tel:0270016157">(02) 123 - 456 - 7890</a></li>
+            <li><a href="tel:+639622530149">(63+) 9924401097</a></li>
+            <li><a href="tel:+639622530149">(63+) 9924401097</a></li>
+            <li><a href="tel:+639622530149">(63+) 9924401097</a></li>
+            <li><a href="mailto:dentalclinic@gmail.com">dentalclinic@gmail.com</a></li>
+          </ul>
+        </div>
+      </div>
+      <hr>
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-sm-6 col-xs-12">
+          <p class="copyright-text">Copyright &copy; 2024 All Rights Reserved
+          </p>
+        </div>
 
+        <div class="col-md-4 col-sm-6 col-xs-12">
+          <ul class="social-icons">
+            <li><a class="facebook" href="mailto:dentalclinic@gmail.com7"><i class="fa-brands fa-facebook-f"></i></a></li>
+            <li><a class="email" href="mailto:dentalclinic@gmail.com"><i class="fa-brands fa-weixin"></i></a></li> 
+            <li><a class="telegram" href="tel:0270016157"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a class="mobile" href="tel:0270016157"><i class="fa-brands fa-viber"></i></a></li>
+            <li><a class="email" href="mailto:dentalclinic@gmail.com"><i class="fa fa-envelope"></i></a></li>   
+            <li><a class="email" href="mailto:dentalclinic@gmail.com"><i class="fa-brands fa-x-twitter"></i></a></li>  
+            <li><a class="email" href="mailto:dentalclinic@gmail.com"><i class="fa-brands fa-skype"></i></a></li>
 
-
+    
+          </ul>
+        </div>
+      </div>
+    </div>
 </footer>
 
-
-
-<div class="copy-right text-center">
-
-    <ul>
-        <li>
-            <p>&copy; 2024 All rights reserved. Website design by Infinitech Advertising Corporation.</p>
-        </li>
-        <li><a href="/privacy-policy">Privacy Policy</a></li>
-        <li><a href="/terms-and-conditions">Terms and Conditions</a></li>
-    </ul>
+{{-- Soc Med Icon --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="floating-socmed">
+    <a href="https://www.facebook.com/profile.php?id=61562146592005" class="fa fa-facebook"></a>
+    <a href="mailto:infinitechcorp.ph@gmail.com" class="fa fa-envelope" style="font-size:30px; background-color:#FAF3F0; color: rgb(22, 22, 22)"></a>
+    <a href="tel:+639622530149" class="fa fa-telegram"></a>
+    <a href="https://www.facebook.com/profile.php?id=61562146592005" class="fa fa-instagram"></a>
+    <a href="tel:0270016157" class="fa fa-phone"></a>
 </div>
+
